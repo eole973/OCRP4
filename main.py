b15814f3452ab744bb2a4bf052ff4f545fb42487
Fontaine <70113775+eole973@users.noreply.github.com>
2023-02-20 17:52:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# coding: utf-8
+from module.Controller import Controller
+
+
+def main():
+    """high level executable"""
+    controller = Controller()
+    controller.run()
+
+
+if __name__ == "__main__":
+    main()
